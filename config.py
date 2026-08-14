4b6e5af254c4528e7abf71f6d79461938300c13d
Michael Steeves <msteevesaudio@gmail.com>
2026-08-14 15:25:38 -0500

    Serve cookie photos from static/images instead of Facebook

--- config.py
+++ config.py
@@ -64,25 +64,25 @@ class Config:
             "chocolate_chip": {
                 "name": "Brown Butter Chocolate Chip",
                 "description": "Rich, nutty brown butter with classic chocolate chips. Soft centers, golden edges.",
-                "img_default": "https://scontent-hou1-1.xx.fbcdn.net/v/t39.30808-6/771844380_122107801077414493_8097330256772646541_n.jpg?stp=dst-jpg_tt6&cstp=mx1216x1171&ctp=s1216x1171&_nc_cat=102&ccb=1-7&_nc_sid=127cfc&_nc_ohc=kulWUEjlmD4Q7kNvwGyyGoE&_nc_oc=AdpIrfjDaGPyOtSuiAro0TIVEsIQ7bLtEW8BRLAykf1jlwaGaUu2panE6UDkwthBEFbkQWqrDX3mrM3d1eKJC57u&_nc_zt=23&_nc_ht=scontent-hou1-1.xx&_nc_gid=ISN1G3P0Cw9SxtszMbyu_g&_nc_ss=7b2a8&oh=00_AQEfwsupbSJuFXxbuYV9NB939pjOo5kdOyFE3TVIxfxWmQ&oe=6A81995E",
-                "img_hover": "https://scontent-hou1-1.xx.fbcdn.net/v/t39.30808-6/773580837_122107572291414493_5895687146750392091_n.jpg?stp=dst-jpg_tt6&cstp=mx1027x1027&ctp=s1027x1027&_nc_cat=104&ccb=1-7&_nc_sid=833d8c&_nc_ohc=jTM62DIscfgQ7kNvwEQHhxA&_nc_oc=Adpx4mxTfxSrYktRY_37fAijO9P-TppsOr34eJ65xy2p2EFB5NLYaAODeCVwa5ScuQGG_ASCqwdownqhF6rSz40L&_nc_zt=23&_nc_ht=scontent-hou1-1.xx&_nc_gid=PSUmQINS5IQg60wX99a5_w&_nc_ss=7b2a8&oh=00_AQFzgoYbCspHdvIC7J6aBeo3wHpBb-qlfqdRDZ1UKC33Yg&oe=6A819E88",
+                "img_default": "images/chocolate_chip.jpg",
+                "img_hover": "",
             },
             "macadamia": {
                 "name": "Macadamia White Chocolate",
                 "description": "Buttery macadamia nuts and smooth white chocolate in every bite.",
-                "img_default": "https://scontent-hou1-1.xx.fbcdn.net/v/t39.30808-6/765929390_122107801035414493_2576688302104320753_n.jpg?stp=dst-jpg_tt6&cstp=mx1129x1102&ctp=s1129x1102&_nc_cat=111&ccb=1-7&_nc_sid=127cfc&_nc_ohc=iiMh4YAcL7UQ7kNvwH0Fza3&_nc_oc=AdrZch64C_YGIuWb0As_QM4W_BOjpdoQZsmzdPq025W3KNr4wwacQ2NuIyrwkNreEgf0KXbWB3R-hOvvgOPHdBuw&_nc_zt=23&_nc_ht=scontent-hou1-1.xx&_nc_gid=barCj6F9gXczR66ytsMVbw&_nc_ss=7b2a8&oh=00_AQH4EDoKFuMHLOYoVBPm1Y3KLzLRwRdTllfJtIO_Jef4xg&oe=6A8188F2",
-                "img_hover": "https://scontent-hou1-1.xx.fbcdn.net/v/t39.30808-6/763018606_122104573833414493_1072829447698767456_n.jpg?stp=dst-jpg_tt6&cstp=mx1168x880&ctp=s1168x880&_nc_cat=106&ccb=1-7&_nc_sid=127cfc&_nc_ohc=XCEz2uADg44Q7kNvwFo6syR&_nc_oc=AdrGilUvYzDVBwEAIkwZ2tmVii4cyplqHEQE1nA4DPFEUhEpcSwdPEQtbKU9sb0bo3HVNCt5k90TfFOSejECNWhb&_nc_zt=23&_nc_ht=scontent-hou1-1.xx&_nc_gid=9voSWnjxFcTCsLMi5Ri43Q&_nc_ss=7b2a8&oh=00_AQERCeYfaMUmFgonzR811sbqeB-5Jmqqdy0eKUi2z2NrUw&oe=6A7E4F02",
+                "img_default": "images/macadamia.jpg",
+                "img_hover": "",
             },
             "salted_caramel": {
                 "name": "Salted Caramel Chocolate Pecan",
                 "description": "Gooey caramel, chocolate, and toasted pecans with a kiss of sea salt.",
-                "img_default": "",
+                "img_default": "images/salted_caramel.jpg",
                 "img_hover": "",
             },
             "peanut_butter": {
                 "name": "White Miso Peanut Butter",
                 "description": "Creamy peanut butter with a gentle savory depth from white miso.",
-                "img_default": "https://scontent-hou1-1.xx.fbcdn.net/v/t39.30808-6/768408728_122107801023414493_1300102988743959417_n.jpg?stp=dst-jpg_tt6&cstp=mx1145x1101&ctp=s1145x1101&_nc_cat=109&ccb=1-7&_nc_sid=127cfc&_nc_ohc=B8FJhFvP_6sQ7kNvwESKmf6&_nc_oc=AdqHk9AVcGTan1HJsmid_xzrEhcmxfPrUjQR51OJ71WWpq5wvJJf4rhv0DIcogOu06-vaUdhf4QeDmVfyMKK5dS5&_nc_zt=23&_nc_ht=scontent-hou1-1.xx&_nc_gid=ahMBgGF5pZMz24yIB4oRyA&_nc_ss=7b2a8&oh=00_AQEeMy2EZLKZLoVLN0NTtSzF7wBoS5pbVb6LMy9I3_pYQw&oe=6A81945C",
-                "img_hover": "https://scontent-hou1-1.xx.fbcdn.net/v/t39.30808-6/760033673_122102208255414493_8939993557948681595_n.jpg?stp=dst-jpegr_tt6&cstp=mx1542x2048&ctp=s1542x2048&_nc_cat=103&ccb=1-7&_nc_sid=cc71e4&_nc_ohc=GB9V2NcDN8sQ7kNvwFepH17&_nc_oc=AdrEhjm5lzw4mxAUIXmIbnYnhLkFum0IsFBXFUWPXW7O66HNM0owWGtGcKMmqp2Hc2mZIE9gNsKfTs0YFHxQrzs6&_nc_zt=23&se=-1&_nc_ht=scontent-hou1-1.xx&_nc_gid=SkMgguI56auPKKqATHYGxw&_nc_ss=7b2a8&oh=00_AQHlKWOfJfl8fhYTp0b2HbdlSWv-TLhE9zpzKeChwq6DVA&oe=6A7E42E0",
+                "img_default": "images/peanut_butter.jpg",
+                "img_hover": "",
             },
         }
